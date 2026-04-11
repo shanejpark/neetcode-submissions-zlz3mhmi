008887a901a3fc3baa138c53d9987d37a17f7468
Shane <49879028+shanejpark@users.noreply.github.com>
2026-04-11 19:36:48 -0400

    Add: minimum-window-with-characters - submission-10

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-10.py
@@ -0,0 +1,30 @@
+from collections import Counter
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(t) > len(s):
+            return ""
+        if t == s:
+            return s
+        
+        resl, resr = -1, -1
+        l = 0
+        havemap, needmap = Counter(), Counter(t)
+        have, need = 0, len(needmap)
+        for r in range(len(s)):
+            if s[r] in t:
+                havemap[s[r]] += 1
+                if havemap[s[r]] == needmap[s[r]]:
+                    have += 1
+
+            while have == need:
+                if (r - l + 1 < resr - resl + 1 or resl == resr == -1):
+                    resl, resr = l, r
+                if s[l] in t:
+                    if havemap[s[l]] == needmap[s[l]]:
+                        have -= 1
+                    havemap[s[l]] -= 1
+                l += 1
+
+        return s[resl:resr+1]
+            
